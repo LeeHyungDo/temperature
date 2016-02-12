97001af09c789143795d958457c6db292a054412
LeeHyungDo <joytop88@gmail.com>
2016-02-12 10:21:09 +0900

    Update checktem.js

--- checktem.js
+++ checktem.js
@@ -1,7 +1,9 @@
+//requirement : npm install dht-sensor --save
 var dht = require('dht-sensor');
 
 var sensor = {
   read : function() {
+    //set RaspberryPI 2 GPIO(11:dht11, 18:BCM GPIO)
     var current = dht.read(11,18);
     console.log('humidity : ' + current.humidity + '% ' + 'temperature: ' + current.temperature + 'C');
     setTimeout(function() {sensor.read();}, 1000);
