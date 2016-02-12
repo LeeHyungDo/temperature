16913b0637571d43fef6f2e50f34e2fff02c1f7d
LeeHyungDo <joytop88@gmail.com>
2016-02-12 10:35:33 +0900

    add blynk library
    view  sensor data from Raspberry Pi2 at smart phone which installed Blynk app.

--- checktem.js
+++ checktem.js
@@ -1,14 +1,26 @@
 //requirement : npm install dht-sensor --save
-var dht = require('dht-sensor');
+//            : npm install blynk-library
+var blynkLib  = require('blynk-library');
+var sensorLib = require('dht-sensor');
 
-var sensor = {
-  read : function() {
-    //set RaspberryPI 2 GPIO(11:dht11, 18:BCM GPIO)
-    var current = dht.read(11,18);
-    console.log('humidity : ' + current.humidity + '% ' + 'temperature: ' + current.temperature + 'C');
-    setTimeout(function() {sensor.read();}, 1000);
-  }
- }
- 
-sensor.read();
+//set AUTH key from Blynk
+var AUTH ='';
+
+//setup Blynk
+var blynk = new blynkLib.Blynk(AUTH);
+
+//setup snesor
+var sensorType = 11; //11 for DHT11, 22 for DHT22 and AM2302
+var sensorPin = 18; //the GPIO pin number for sensor signal
+
+//Automatically update sensor value every 2 seconds
+setInterval = {function() {
+    var readout = sensorLib.read(sensorType, sensorPin);
+    blynk.virtualWrite(3, readout.temperature.toFixed(1));
+    blynk.virtualWrite(4, readout.humidity.toFixed(1));
+    
+    console.log('humidity : ' + readout.humidity + '% ' + 'temperature: ' + readout.temperature + 'C');
+  }, 2000);
+
+
   
